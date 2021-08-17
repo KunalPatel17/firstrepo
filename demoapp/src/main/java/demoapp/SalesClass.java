fe672bb2b5628e7704817027a54e9265e5e6e4d6
kupatel <kupatel@LIN51013525.corp.capgemini.com>
2021-08-17 10:59:12 +0530

    Display method updated

--- demoapp/src/main/java/demoapp/SalesClass.java
+++ demoapp/src/main/java/demoapp/SalesClass.java
@@ -4,8 +4,12 @@ public class SalesClass {
 	
 	public void display() {
 		int[] arr= {1,3,45,56,90};
-		for(int num:arr)
-			System.out.println(num);
+		int sum = 0;
+		for(int i=0;i<arr.length;i++) {
+			sum+=arr[i];
+			System.out.println(arr[i]);
+		}
+		System.out.println("Sumatiom:" + sum );
 	}
 	
 	public void print() {
